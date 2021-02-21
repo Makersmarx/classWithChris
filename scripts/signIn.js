e062eee3f00312036f7491e1ed5c7d46960d9808
makersmarx <bbiehn1@gmail.com>
2021-02-20 18:27:30 -0600

    add more style and JS

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -1,5 +1,60 @@
-let logIn = document.getElementById('btn');
-let signUp = document.getElementById('btn-sgnup');
-document.addEventListener('click', () => {
+// variables
+const logIn = document.getElementById('btn');
+const signUp = document.getElementById('btn-sgnup');
+const titleHover = document.getElementById('title');
+
+// event listeners
+signUp.addEventListener('click', () => {
   console.log('test');
+  window.location.href = '../signUp/signup.html';
 });
+
+logIn.addEventListener('click', () => {
+  console.log('yep');
+});
+
+// titleHover.addEventListener('mouseover', () => {
+//   titleHover.style.fontSize = '25px';
+// });
+
+// modal
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+const modalSpan = document.getElementById('close');
+
+// When the user clicks on the button, open the modal
+logIn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+// When the user clicks on <span> (x), close the modal
+modalSpan.addEventListener('click', () => {
+  console.log('working?');
+  modal.style.display = 'none';
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+//check local storage for saved user data
+
+const checkUser = () => {
+  const checkName = localStorage.getItem('userName');
+  const checkPassword = localStorage.getItem('userPassword');
+
+  const userName = document.getElementById('username');
+  const userPassword = document.getElementById('password');
+
+  if (userName.value === checkName && userPassword.value === checkPassword) {
+    window.location.href = 'index.html';
+    return false;
+  } else {
+    alert('Error on login');
+  }
+};
